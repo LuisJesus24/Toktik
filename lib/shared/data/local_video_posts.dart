020bcec749e3dcc5db5d453bf38dcb61024aab20
Luis Jesus <luis.j.fcfm@gmail.com>
2025-07-28 18:37:49 -0600

    creacion de aplicacion clon de tiktok

---
+++ lib/shared/data/local_video_posts.dart
@@ -0,0 +1,46 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Minato vs tobi',
+    'videoUrl': 'assets/videos/1.mp4',
+    'likes': 23230,
+    'views': 1523,
+  },
+  {
+    'name': 'Apreciación de personajes',
+    'videoUrl': 'assets/videos/2.mp4',
+    'likes': 24230,
+    'views': 1343,
+  },
+  {
+    'name': 'Highlight intro',
+    'videoUrl': 'assets/videos/3.mp4',
+    'likes': 21564320,
+    'views': 123563,
+  },
+  {
+    'name': 'Saitama, el hereo mas grande',
+    'videoUrl': 'assets/videos/4.mp4',
+    'likes': 320,
+    'views': 2300,
+  },
+  {
+    'name': 'Por que Uzui?',
+    'videoUrl': 'assets/videos/5.mp4',
+    'likes': 3230,
+    'views': 31030,
+  },
+  {
+    'name': 'La historia que necesitabamos',
+    'videoUrl': 'assets/videos/6.mp4',
+    'likes': 10,
+    'views': 330,
+  },
+  {
+    'name': 'El gol de Nagi',
+    'videoUrl': 'assets/videos/7.mp4',
+    'likes': 1320,
+    'views': 33032,
+  },
+
+
+];
